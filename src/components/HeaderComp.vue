af022f0b2fe34eaa928dff4e79ffed349a3ac9b9
GioCri01 <giovanni.crispino94@icloud.com>
2022-05-17 16:08:27 +0200

    Struttura esercizio

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <img src="https://icones.pro/wp-content/uploads/2021/04/logo-spotify-icone-original.png" alt="logo">
+    <div class="custom-select" >
+  <select>
+    <option value="0">Seleziona un genere:</option>
+    <option value="1">Rock</option>
+    <option value="2">blues</option>
+    <option value="3">Pop</option>
+  </select>
+</div>
+    
+  </header>
+</template>
+
+<script>
+export default {
+    name:"HeaderComp",
+
+}
+</script>
+
+<style lang="scss" scoped>
+  header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 100px;
+    background-color: #283038;
+    img{
+      width: 80px;
+      padding: 15px;
+    }
+  }
+  .custom-select{
+    padding: 15px;
+    select{
+      width: 200px;
+      height: 40px;
+    }
+  }
+
+
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+      <div class="container"></div>
+  </main>
+</template>
+
+<script>
+export default {
+    name:"MainComp",
+
+}
+</script>
+
+<style lang="scss" scoped>
+main{
+    width: 100%;
+    height:calc(100vh - 100px);
+    background-color:#1e2d3b ;
+}
+
+
+</style>
