d6cd1a9b173d0ed6161489b18aa042320feebb9e
GioCri01 <giovanni.crispino94@icloud.com>
2022-05-17 17:48:50 +0200

    sezione card con chiamata Api e stampa con props

---
+++ src/components/CardComp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="box">
+      <img :src="cardItem.poster" alt="">
+      <span class="title">{{cardItem.title}}</span>
+      <span class="author">{{cardItem.author}}</span>
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+    name:"CardComp",
+   
+
+    props:{
+        cardItem: Object
+    }
+
+    
+
+}
+</script>
+
+<style lang="scss" scoped>
+.box{
+    width: calc(100%/6);
+    background-color: #2e3a46;
+    margin: 20px;
+    padding: 15px;
+    img{
+        width: 100%;
+    }
+}
+
+</style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,21 +1,63 @@
 <template>
   <main>
-      <div class="container"></div>
+      <div class="container card-container">
+          <CardComp
+          v-for="(item ,index) in arrCards"
+          :key="`item-${index}`"
+          :cardItem="item"
+          
+          />
+      </div>
   </main>
 </template>
 
 <script>
-export default {
-    name:"MainComp",
+import axios from "axios";
+import CardComp from './CardComp.vue';
 
+export default {
+    name: "MainComp",
+
+    data(){
+        return{
+            baseURL:"https://flynn.boolean.careers/exercises/api/array/music",
+            arrCards :[],
+        }
+    },
+
+    components: { CardComp },
+
+    mounted(){
+        this.getAPI()
+    },
+
+    methods:{
+        getAPI(){
+            axios.get(this.baseURL)
+            .then(res =>{
+                this.arrCards = res.data.response;
+                
+
+            })
+
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 main{
+    
     width: 100%;
     height:calc(100vh - 100px);
     background-color:#1e2d3b ;
+    .card-container{
+        padding: 20px;
+        display: flex;
+        
+        flex-wrap: wrap;
+        
+    }
 }
 
 
